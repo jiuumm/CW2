639f66d6ecf1c30a690aaebaace148aef2c01737
jiuumm <jiyoung6301@gmail.com>
2023-10-11 11:31:51 +0900

    Create 완전이진트리.py

---
+++ Yoo.C/2기/완전이진트리.py
@@ -0,0 +1,20 @@
+import sys
+input  = sys.stdin.readline
+n= int(input())
+lst = list(map(int, input().split()))
+
+tree = [[]for _ in range(n)]
+
+
+def dfs(lst, l):
+    mid = (len(lst)//2)
+    tree[l].append(lst[mid])
+    if len(lst)==1:
+        return
+    dfs(lst[:mid], l+1)
+    dfs(lst[mid+1:l+1])
+    
+dfs(lst,0)
+
+for ele in tree:
+    print(*ele)
